c9eeba3af081483f96be33b625fba26a86b3a45f
Anatoliy Cherevach <anatoliycherevach@gmail.com>
2021-03-04 01:33:41 +0200

    Lab 1.1

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+function MainScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ textAlign: 'center', fontSize: 20 }}>
+          {`Черевач Анатолій\n Група ІП-83\n ЗК ІП-8525`}
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SecondScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ textAlign: 'center', fontSize: 20 }}>
+        {`Second Screen` }
+
+      </Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+
+            let iconName;
+
+            if (route.name === 'Main') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Second') {
+              iconName = focused ? 'document' : 'document-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#000000',
+          inactiveTintColor: '#696969',
+        }}
+      >
+        <Tab.Screen name="Main" component={MainScreen} />
+        <Tab.Screen name="Second" component={SecondScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
